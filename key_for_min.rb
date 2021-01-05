2c5f30aa30525661b6fe7981128e1221db273a31
Taylor Treece <tayloratreece@gmail.com>
2021-01-05 19:48:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash == nil
-    return nil
-  else
-    min_value = nil
-    name_hash.each do |key, value|
-    if value < min_value
-      return key
-      end
+  min_value = nil
+  min_key = nil
+  name_hash.each do |k, v|
+    if min_value == nil
+      min_value = v
+      min_key = k
+    elsif min_value > v
+      min_value = v
+      min_key = k
     end
   end
+  min_key
 end
