265bf9eed6858cce05a6e814de347806ef88d247
Taylor Treece <tayloratreece@gmail.com>
2021-01-05 19:12:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_value = nil
   min_key = nil
   name_hash.each do |k, v|
-    if min_value == nil || v > min value
+    if min_value == nil || v > min_value
       min_value = v
       min_key = k
     end
