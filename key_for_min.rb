0d60e0a66a8b96f8d24552ecad55ccb408cf3a1d
Taylor Treece <tayloratreece@gmail.com>
2021-01-05 19:43:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   else
     min_value = nil
     name_hash.each do |key, value|
-    if min_value < value
+    if min_value > value
       return key
       end
     end
